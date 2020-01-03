d948bf3bb0eb5bc4234f613da11963a1bd8c0c9a
gehongming <1010562639@qq.com>
2020-01-03 18:13:38 +0800

    B2B项目

--- Testcases/all/conftest.py
+++ Testcases/all/conftest.py
@@ -14,8 +14,8 @@ from common.delete_history import *
 
 remove_files_in_dir(contants.reports_log)
 print('删除日志')
-remove_files_in_dir(contants.allure_results_dir)
-print('删除xml')
+# remove_files_in_dir(contants.allure_results_dir)
+# print('删除xml')
 
 
 # session级别的
