e5d44500f7dfb85c00c199114b52d4ce48f2b8b4
gehongming <1010562639@qq.com>
2020-01-03 16:14:12 +0800

    B2B项目

--- Testcases/all/main.py
+++ Testcases/all/main.py
@@ -1,8 +1,14 @@
 import pytest
 import os
+import sys
+sys.path.append('./')
+
 
 # pytest.main(["-v","-m","demo","--reruns","1","--alluredir=../OutPuts/allure-results"])
 # os.system(r"allure generate --clean ../OutPuts/allure-results -o ../allure-report ")
+
+
+
 
 # pytest.main(["-v","-m","buy","--alluredir=../../OutPuts/allure-results"])
 # os.system(r"allure generate --clean ../../OutPuts/allure-results -o ../../allure-report ")
