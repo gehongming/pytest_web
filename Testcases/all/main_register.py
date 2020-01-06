391b3f886937e864807409db5a2f96780b67748a
gehongming <1010562639@qq.com>
2020-01-06 20:21:48 +0800

    修改main

--- Testcases/all/main_register.py
+++ Testcases/all/main_register.py
@@ -6,5 +6,5 @@ sys.path.append('./')
 
 
 if __name__ == '__main__':
-    pytest.main(["-v","-m","register","--alluredir=../../OutPuts/allure-results",'--workers=1','--tests-per-worker=4'])
+    pytest.main(["-v","-m","register","--alluredir=../../OutPuts/allure-results",'--workers=2','--tests-per-worker=4'])
     os.system(r"allure generate --clean ../../OutPuts/allure-results -o ../../allure-report ")
