741e56d1a19d7fd967f058e1ef80ca7c84160418
gehongming <1010562639@qq.com>
2020-01-16 00:19:05 +0800

    修改执照位置

--- Testdates/m/registered_datas.py
+++ Testdates/m/registered_datas.py
@@ -9,4 +9,4 @@ zhanghao_data={"phone":a,"code":"sD2q"}
 
 
 store_data={"storename":'小明眼镜cs'+a,"address":"网管查房","contact":"小明"
-            ,"b_address":"F:\\工作\\B2B专用\\营业执照.jpeg","m_address":"F:\\工作\\B2B专用\\医疗许可证.jpg"}
+            ,"b_address":"D:\\B2B\\营业执照.jpeg","m_address":"D:\\B2B\\医疗许可证.jpg"}
