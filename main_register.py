4c54ce75338878f361102719a6aa6fe199d4f524
gehongming <1010562639@qq.com>
2020-01-17 00:19:11 +0800

    注册增加重运行机制

--- main_register.py
+++ main_register.py
@@ -7,7 +7,7 @@ sys.path.append('./')
 
 if __name__ == '__main__':
 
-    pytest.main(["-v","-m","register","--alluredir=OutPuts/allure-results"])
+    pytest.main(["-v","-m","register","--reruns","2","-delay","5","--alluredir=OutPuts/allure-results"])
     os.system("allure generate  OutPuts/allure-results -o allure-report --clean")
 
 
