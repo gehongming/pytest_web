0abcfb8b8d36a8dc149af1a93b78029084d58ae7
gehongming <1010562639@qq.com>
2020-02-05 18:47:20 +0800

    修改验证码

--- Testdates/m/registered_datas.py
+++ Testdates/m/registered_datas.py
@@ -5,8 +5,8 @@ from common.create_data import *
 
 
 a=create_phone()
-zhanghao_data={"phone":a,"code":"sD2q"}
+zhanghao_data={"phone":a,"code":"YjQ1"}
 
 
-store_data={"storename":'小明眼镜cs'+a,"address":"网管查房","contact":"小明"
-            ,"b_address":"D:\\B2B\\营业执照.jpeg","m_address":"D:\\B2B\\医疗许可证.jpg"}
+store_data={"storename":'小明眼镜cs'+a,"address":"网管查房","contact":"小明",
+            "b_address":"F:\\B2B\\营业执照.jpeg","m_address":"F:\\B2B\\医疗许可证.jpg"}
